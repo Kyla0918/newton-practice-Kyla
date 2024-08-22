4bd2aa3819e6dc9d3f92bf8acb4a9e34405f600a
siqiqiang <siqi_qiang@berkeley.edu>
2024-08-22 11:24:04 -0700

    Add basic implementation of Newton's method.

---
+++ newton.py
@@ -0,0 +1,20 @@
+e = 0.0001
+
+def f(x):
+    y = x*x + 2*x + 1
+    return y
+
+def fderi(x):
+    e = 0.0001
+    y = (f(x+e)-f(x))/e
+    return(y)
+
+def fderi2(x):
+    e = 0.0001
+    y = (fderi(x+e)-fderi(x))/e
+    return(y)
+
+def optimize(x0):
+    x1 = x0
+    while abs(x0-x1) < 0.0001:
+        x1 = x0 - fderi(x0)/fderi2(x0)
